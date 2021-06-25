013b91d8277f8b4bbd46fbabcb60666d2b5698b4
Elperson <40611324+Elpersonn@users.noreply.github.com>
2021-06-25 12:28:05 +0200

    Update install.sh

--- Uploader/install.sh
+++ Uploader/install.sh
@@ -7,5 +7,6 @@ apt install -y nano --fix-missing
 apt install -y luarocks --fix-missing
 luarocks install bcrypt
 luarocks install lapis # Updating the lapis version.
+luarocks install lua-resty-mail
 mkdir -p /srv/lapis/html/files # This doesn't work I think... ?
 chmod a=rw /srv/lapis/html/files
